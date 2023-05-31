694680df587109d58af11fc66feffc0b91534316
chllamas <chrisllamas2000@gmail.com>
2023-05-31 15:41:29 -0700

    Finished day4

---
+++ day4/main.ml
@@ -0,0 +1,24 @@
+let has_subset (a, b) (c, d) =
+    (c >= a && d <= b) || (a >= c && b <= d)
+
+let process_line line =
+    let range1, range2 = Scanf.sscanf line "%d-%d,%d-%d" (fun w1 w2 w3 w4 -> (w1, w2), (w3, w4)) in
+    if has_subset range1 range2 then 1 else 0
+
+let read_input filename =
+    let ic = open_in filename in 
+    let rec loop acc =
+        try 
+            let line = input_line ic in
+            let result = process_line line in
+            loop (acc + result)
+        with End_of_file ->
+            close_in ic;
+            acc
+    in
+    let total = loop 0 in
+    print_int total;
+    print_newline ()
+
+let () =
+    read_input "input.txt"
