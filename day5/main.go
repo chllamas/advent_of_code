55efaea2c3ebaca20596d63661f82fcf52ece0d1
chllamas <chrisllamas2000@gmail.com>
2023-06-01 21:11:59 -0700

    Finished part 1 of day 5

--- day5/main.go
+++ day5/main.go
@@ -1,56 +1,85 @@
-package main 
+package main
 
 import (
-    "os"
-    "strconv"
-    "bufio"
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
 )
 
 type Stack struct {
     contents []byte 
 }
 
-func (s *Stack) pop() byte {
+func (s *Stack) pop() (byte, error) {
     if len(s.contents) == 0 {
-        panic("Stack underflow!")
+        return 0, errors.New("bad")
     }
-    ret := s.contents[len(s.contents) - 1]
-    s.contents = s.contents[:len(s.contents) - 1]
-    return ret
+    ret := s.contents[0]
+    s.contents = s.contents[1:]
+    return ret, nil
 }   
 
 func (s *Stack) push(b byte) {
-    s.contents = append(s.contents, b)
+    s.contents = append([]byte{b}, s.contents...)
+}
+
+func printTopVals(stacks *[9]Stack) {
+    for _, stack := range *stacks {
+        if t, err := stack.pop(); err == nil {
+            fmt.Printf("%s", string(t))
+        }
+    }
+    fmt.Println()
 }
 
 func main() {
+    /*
+    [B]                     [N]     [H]
+    [V]         [P] [T]     [V]     [P]
+    [W]     [C] [T] [S]     [H]     [N]
+    [T]     [J] [Z] [M] [N] [F]     [L]
+    [Q]     [W] [N] [J] [T] [Q] [R] [B]
+    [N] [B] [Q] [R] [V] [F] [D] [F] [M]
+    [H] [W] [S] [J] [P] [W] [L] [P] [S]
+    [D] [D] [T] [F] [G] [B] [B] [H] [Z]
+    1   2   3   4   5   6   7   8   9 
+    */
+    stacks := [9]Stack{
+        {contents: []byte("BVWTQNHD")},
+        {contents: []byte("BWD")},
+        {contents: []byte("CJWQST")},
+        {contents: []byte("PTZNRJF")},
+        {contents: []byte("TSMJVPG")},
+        {contents: []byte("NTFWB")},
+        {contents: []byte("NVHFQDLB")},
+        {contents: []byte("RFPH")},
+        {contents: []byte("HPNLBMSZ")},
+    }
+
     file, err := os.Open("input.txt")
     if err != nil {
         panic("couldn't open file")
     }
+    defer file.Close()
 
-    /*
-     [B]                     [N]     [H]
-     [V]         [P] [T]     [V]     [P]
-     [W]     [C] [T] [S]     [H]     [N]
-     [T]     [J] [Z] [M] [N] [F]     [L]
-     [Q]     [W] [N] [J] [T] [Q] [R] [B]
-     [N] [B] [Q] [R] [V] [F] [D] [F] [M]
-     [H] [W] [S] [J] [P] [W] [L] [P] [S]
-     [D] [D] [T] [F] [G] [B] [B] [H] [Z]
-      1   2   3   4   5   6   7   8   9 
-    */
-  
-    // switch to ()
-    stacks := [9]Stack{
-        {contents: []byte{"BVWTQNHD"}},
-        {contents: []byte{"BWD"}},
-        {contents: []byte{"CJWQST"}},
-        {contents: []byte{"PTZNRJF"}},
-        {contents: []byte{"TSNMJVPG"}},
-        {contents: []byte{"NTFWB"}},
-        {contents: []byte("NVHFQDLB")},
-        {contents: []byte{"RFPH"}},
-        {contents: []byte{"HPNLBMSZ"}},
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := scanner.Text()
+        var n, from, to int
+
+        _, err := fmt.Sscanf(line, "move %d from %d to %d", &n, &from, &to)
+        if err != nil {
+            panic(fmt.Sprintf("Couldn't parse line: %s", line))
+        }
+
+        for i := 0; i < n; i++ {
+            t, err := stacks[from-1].pop()
+            if err == nil {
+                stacks[to-1].push(t)
+            }
+        }
     }
+
+    printTopVals(&stacks)
 }
