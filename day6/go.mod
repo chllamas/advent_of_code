8159d9ef11ae81057910ba856a326b251534238a
chris <chrisllamas2000@gmail.com>
2023-06-02 20:58:01 +0000

    Finished day 6

---
+++ day6/go.mod
@@ -0,0 +1,3 @@
+module example.com/advent_day6
+
+go 1.20
---
+++ day6/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "os"
+    "bufio"
+    "fmt"
+)
+
+func validSubstring(s string) bool {
+    charSet := make(map[rune]bool)
+
+    for _,c := range s {
+        if charSet[c] {
+            return false
+        }
+        charSet[c] = true
+    }
+
+    return true
+}
+
+func main() {
+    file, err := os.Open("input.txt")
+    if err != nil {
+        panic("Couldn't open file")
+    }
+
+    scanner := bufio.NewScanner(file)
+    scanner.Scan()
+    text := scanner.Text()
+    h := 0
+    marker := 4
+    for !validSubstring(text[h:marker]) {
+        marker += 1
+        h += 1
+    }
+
+
+    fmt.Printf("Marker after %d\n", marker)
+}
