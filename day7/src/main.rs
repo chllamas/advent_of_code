3cfdb0ec8688bf7f61f571fc55be874a44b03a76
chllamas <chrisllamas2000@gmail.com>
2023-06-04 08:45:27 -0700

    day7 is a little more complex than expected but it's going great

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -8,25 +8,6 @@ enum FileNode {
     Doc(i32),
 }
 
-fn retrieve_current_node<'a>(path: &[String], root_node: &'a mut FileNode) -> &'a mut FileNode {
-    let mut current_node = root_node;
-    for dir_name in path {
-        if let FileNode::Dir(dir_map) = current_node {
-            current_node = dir_map
-                .entry(dir_name.clone())
-                .or_insert(FileNode::Dir(HashMap::new()))
-        } else {
-            panic!("Reached unexpected error while retrieving node")
-        }
-    }
-
-    current_node
-}
-
-fn handle_cd() {
-    todo!()
-}
-
 fn handle_ls() {
     todo!() 
 }
@@ -34,16 +15,31 @@ fn handle_ls() {
 fn main() -> std::io::Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
-    let root_node = FileNode::Dir(HashMap::new());
+    let mut root_node = FileNode::Dir(HashMap::new());
 
     let mut current_path: Vec<String> = vec![];
-    let mut current_node: &FileNode = &root_node;
+    let mut current_node: &mut FileNode = &mut root_node;
 
     for line in reader.lines() {
         let line: String = line?;
         match &line[..4] {
-            "$ cd" => todo!(),
-            "$ ls" => todo!(),
+            "$ cd" => line.split('/')
+                .for_each(|p|
+                    match p {
+                        ".." => { current_path.pop(); },
+                        _   => { current_path.push(p.to_string()); },
+                    }
+                ),
+            "$ ls" => {
+                current_node = &mut root_node;
+                for dir_name in &current_path {
+                    if let FileNode::Dir(dir_map) = current_node {
+                        current_node = dir_map 
+                            .entry(dir_name.clone())
+                            .or_insert(FileNode::Dir(HashMap::new()))
+                    }
+                }
+            },
             "dir " => todo!(),
               _    => todo!(),
         }
