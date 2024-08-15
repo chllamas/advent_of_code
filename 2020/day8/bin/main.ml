7d14d3156bc10680676690da09f0055828383812
chllamas <chrisllamas2000@gmail.com>
2024-08-15 16:52:52 -0700

    updates

--- 2020/day8/bin/main.ml
+++ 2020/day8/bin/main.ml
@@ -1,9 +1,22 @@
+let rec find_loop arr acc i =
+        match arr.(i) with
+        | (_,_,true) -> acc
+        | (a, j, _) -> arr.(i) <- (a, j, true);
+                find_loop arr (acc+a) j
+
+(* Array stores (accumulation_add, next_i_index, is_visited_bool) *)
 let () =
-        let rec aux fp acc i =
+        let rec aux fp arr i =
                 try
-                        let line = input_line fp in
-                        acc
+                        arr.(i) <- (Scanf.sscanf (input_line fp) "%s %c%d" (fun cmd sign amt ->
+                                match cmd with
+                                | "nop" -> (0, i+1, false)
+                                | "acc" -> ((if sign == '-' then amt * (-1) else amt), i+1, false)
+                                | "jmp" -> (0, (i + (if sign == '-' then amt * -1 else amt)), false)
+                                | _ -> print_endline "unexpected error"; exit 1
+                        ));
+                        aux fp arr (i+1)
                 with
-                        End_of_file -> acc
+                        End_of_file -> find_loop arr 0 0
         in
-        Printf.printf "Acc value: %d\n" (aux (open_in "input.txt") 0)
+        Printf.printf "Acc value: %d\n" (aux (open_in "input.txt") (Array.init 608 (fun _ -> (0, 0, false))) 0)
