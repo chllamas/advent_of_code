4e5c22db1bb2b68c749118031e5cdf65f4dc08c5
chllamas <chrisllamas2000@gmail.com>
2024-08-15 14:31:52 -0700

    updates

--- 2020/day8/bin/main.ml
+++ 2020/day8/bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+let () =
+        let rec aux fp acc i =
+                try
+                        let line = input_line fp in
+                        acc
+                with
+                        End_of_file -> acc
+        in
+        Printf.printf "Acc value: %d\n" (aux (open_in "input.txt") 0)
