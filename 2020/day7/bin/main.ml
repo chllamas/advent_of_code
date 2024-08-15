066b9d03f87601f9465413e4c67b80f8d109677b
chllamas <chrisllamas2000@gmail.com>
2024-08-15 14:24:01 -0700

    updates

--- 2020/day7/bin/main.ml
+++ 2020/day7/bin/main.ml
@@ -1,22 +1,32 @@
 let rec find_gold hash lst =
         match lst with
         | [] -> false
-        | t :: ts -> t != "nother" && (t == "shinygold" || (match Hashtbl.find hash t with
-                | exception Not_found -> find_gold hash ts
-                | other -> find_gold hash other || find_gold hash ts));;
+        | t :: ts -> Printf.printf "Found %s\n" t; (t == "shinygold" || (
+                try
+                        let other = Hashtbl.find hash t in
+                        find_gold hash other || find_gold hash ts
+                with
+                        Not_found -> find_gold hash ts
+        ));;
 
 let () =
         let rec aux fp hash =
-                match input_line fp with
-                | exception End_of_file -> Hashtbl.fold (fun _ lst acc -> acc + (if find_gold hash lst then 1 else 0)) hash 0
-                | line -> let (key, vals) = Scanf.sscanf line "%s %s bags contain %s@." (fun a b rest -> let items = String.split_on_char ',' rest in
-                        ((a ^ b), List.map String.trim items))
-                        in
-                        List.iter (fun str -> Scanf.sscanf str "%d %s %s bags" (fun _ word1 word2 -> Hashtbl.replace hash key 
-                                (match Hashtbl.find hash str with
-                                        | exception Not_found -> [word1 ^ word2]
-                                        | ts -> (word1 ^ word2) :: ts)))
-                                vals;
+                try
+                        let (key, vals) = Scanf.sscanf (input_line fp) "%s %s bags contain %s@." (fun a b rest -> (a ^ b, List.map String.trim (String.split_on_char ',' rest))) in
+                        List.iter (fun str -> 
+                                Scanf.sscanf str "%d %s %s bags" (fun _ a b ->
+                                        Hashtbl.replace hash key 
+                                                (let v = a ^ b in
+                                                        try
+                                                                let lst = Hashtbl.find hash key in
+                                                                v :: lst
+                                                        with
+                                                                Not_found -> [v]
+                                                )
+                                )
+                        ) vals;
                         aux fp hash
+                with
+                        End_of_file -> Hashtbl.fold (fun _ lst acc -> acc + (if find_gold hash lst then 1 else 0)) hash 0
         in
         Printf.printf "Number of bags: %d\n" (aux (open_in "input.txt") (Hashtbl.create 500))
