5b90c1c0f94b009df1b8dfa1fcd2e1890aa1adb1
chllamas <chrisllamas2000@gmail.com>
2024-08-15 20:23:08 -0700

    updates

--- 2020/day8/_build/default/bin/main.ml
+++ 2020/day8/_build/default/bin/main.ml
@@ -1,1 +1,22 @@
-let () = print_endline "Hello, World!"
+let rec find_loop arr acc i =
+        match arr.(i) with
+        | (_,_,true) -> acc
+        | (a, j, _) -> arr.(i) <- (a, j, true);
+                find_loop arr (acc+a) j
+
+(* Array stores (accumulation_add, next_i_index, is_visited_bool) *)
+let () =
+        let rec aux fp arr i =
+                try
+                        arr.(i) <- (Scanf.sscanf (input_line fp) "%s %c%d" (fun cmd sign amt ->
+                                match cmd with
+                                | "nop" -> (0, i+1, false)
+                                | "acc" -> ((if sign == '-' then amt * (-1) else amt), i+1, false)
+                                | "jmp" -> (0, (i + (if sign == '-' then amt * -1 else amt)), false)
+                                | _ -> print_endline "unexpected error"; exit 1
+                        ));
+                        aux fp arr (i+1)
+                with
+                        End_of_file -> find_loop arr 0 0
+        in
+        Printf.printf "Acc value: %d\n" (aux (open_in "input.txt") (Array.init 608 (fun _ -> (0, 0, false))) 0)
--- 2020/day8/bin/main.ml
+++ 2020/day8/bin/main.ml
@@ -1,22 +1,44 @@
-let rec find_loop arr acc i =
+(*let rec find_loop arr acc i =
         match arr.(i) with
-        | (_,_,true) -> acc
-        | (a, j, _) -> arr.(i) <- (a, j, true);
-                find_loop arr (acc+a) j
+        | (_, _,_,true) -> acc
+        | (s, a, j, _) -> arr.(i) <- (s, a, j, true);
+                find_loop arr (acc+a) j;;*)
 
-(* Array stores (accumulation_add, next_i_index, is_visited_bool) *)
-let () =
+let rec validate_loop arr acc i =
+        if i >= 608 then
+                Some(acc)
+        else
+                match arr.(i) with
+                | (_,_,true) -> None
+                | (s,x,_) -> arr.(i) <- (s,x,true);
+                        match s with
+                        | "nop" -> validate_loop arr acc (i+1)
+                        | "jmp" -> validate_loop arr acc (i+x)
+                        | _ -> validate_loop arr (acc+x) (i+1)
+
+
+let create_array () =
         let rec aux fp arr i =
                 try
                         arr.(i) <- (Scanf.sscanf (input_line fp) "%s %c%d" (fun cmd sign amt ->
-                                match cmd with
-                                | "nop" -> (0, i+1, false)
-                                | "acc" -> ((if sign == '-' then amt * (-1) else amt), i+1, false)
-                                | "jmp" -> (0, (i + (if sign == '-' then amt * -1 else amt)), false)
-                                | _ -> print_endline "unexpected error"; exit 1
+                                (cmd, (if sign == '-' then amt * (-1) else amt), false)
                         ));
                         aux fp arr (i+1)
                 with
-                        End_of_file -> find_loop arr 0 0
+                        End_of_file -> arr
         in
-        Printf.printf "Acc value: %d\n" (aux (open_in "input.txt") (Array.init 608 (fun _ -> (0, 0, false))) 0)
+        aux (open_in "input.txt") (Array.init 608 (fun _ -> ("nop", 0, false))) 0;;
+
+(* Array stores (accumulation_add, next_i_index, is_visited_bool) *)
+let () =
+        let rec aux arr i =
+                let matches () = match validate_loop arr 0 0 with
+                        | Some(x) -> x
+                        | None -> aux (create_array ()) (i+1)
+                in
+                match arr.(i) with
+                | ("nop",a,b) -> arr.(i) <- ("jmp",a,b); matches ()
+                | ("jmp",a,b) -> arr.(i) <- ("nop",a,b); matches ()
+                | _ -> aux arr (i+1)
+        in
+        Printf.printf "Acc value: %d\n" (aux (create_array ()) 0)
