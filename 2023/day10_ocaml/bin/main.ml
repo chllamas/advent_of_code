82233137901c20c8c06a75c0c54dcf012ac2ad90
chllamas <chrisllamas2000@gmail.com>
2024-09-11 11:15:18 -0700

    updates

--- 2023/day10_ocaml/bin/main.ml
+++ 2023/day10_ocaml/bin/main.ml
@@ -1,40 +1,39 @@
-let file_name = "test.txt"
+let file_name = "test3.txt"
 
 let create_graph str x_len y_len =
-  let up i = if i >= x_len then Some (i - x_len) else None
-  and down i = if i < x_len * (x_len - 1) then Some (i + x_len) else None
-  and left i = if i mod x_len > 0 then Some (i - 1) else None
-  and right i = if i mod x_len < x_len - 1 then Some (i + 1) else None in
+  let up i = i - x_len
+  and down i = i + x_len
+  and left i = i - 1
+  and right i = i + 1 in
   Array.init (x_len * y_len) (fun i ->
       match str.[i] with
       | 'F' -> (right i, down i, -1)
       | 'L' -> (up i, right i, -1)
       | 'J' -> (up i, left i, -1)
       | '7' -> (down i, left i, -1)
-      | 'S' -> (down i, up i, 0)
+      | 'S' -> (down i, right i, 0)
       | '|' -> (up i, down i, -1)
       | '-' -> (left i, right i, -1)
-      | '.' -> (None, None, -1)
+      | '.' -> (-1, -1, -1)
       | _ -> failwith "Unknown character in input")
 
 let parse_graph graph =
   let queue = Queue.create () in
   let rec aux max_dist =
-    let validate_neighbor node_opt dist =
-      if Option.is_some node_opt then
-        let node = Option.get node_opt in
-        match graph.(node) with
-        | a, b, -1 ->
-            graph.(node) <- (a, b, dist + 1);
-            Queue.push node queue
-        | _ -> ()
+    let validate_neighbor dist node =
+      match graph.(node) with
+      | a, b, -1 ->
+          graph.(node) <- (a, b, dist + 1);
+          Queue.push node queue
+      | _ -> ()
     in
     match try Some graph.(Queue.pop queue) with Queue.Empty -> None with
     | Some (a, b, dist) ->
-        validate_neighbor a dist;
-        validate_neighbor b dist;
+        let v = validate_neighbor dist in
+        v a;
+        v b;
         aux (max max_dist dist)
-    | _ -> max_dist
+    | None -> max_dist
   in
   Queue.push
     (Option.get (graph |> Array.find_index (fun (_, _, x) -> x == 0)))
@@ -57,23 +56,33 @@ let () =
   |> Printf.printf "Furthest point is %d\n"
   *)
 
-let raycast graph row_len i =
-  let rec aux i res dt =
+let print_graph graph x_len =
+  let rec aux i =
     match try Some graph.(i) with Invalid_argument _ -> None with
-    | Some (_, _, d) when d > -1 ->
-        if dt >= 0 && abs (dt - d) == 1 then aux (i - row_len) res d
-        else aux (i - row_len) (res + 1) d
-    | Some _ -> aux (i - row_len) res (-1)
-    | None -> res mod 2 == 1
+    | Some (_, _, -1) ->
+        if i mod x_len == 0 then print_newline ();
+        print_char '.';
+        aux (i + 1)
+    | Some _ ->
+        if i mod x_len == 0 then print_newline ();
+        print_char '#';
+        aux (i + 1)
+    | None -> print_newline ()
   in
-  let res = aux i 0 (-1) in
-  if res then
-    let _ =
-      Printf.printf "Coord (%d) enclosed and has value dist of %d\n" i
-        (match graph.(i) with _, _, d -> d)
-    in
-    res
-  else res
+  aux 0
+
+let raycast graph x i =
+  let rec aux i res dt =
+    match graph.(i) with
+    | _, _, d when d > -1 && i mod x > 0 ->
+        if dt == -1 || abs (dt - d) != 1 then aux (i - 1) (res + 1) d
+        else aux (i - 1) res d
+    | _, _, d when d > -1 && i mod x == 0 && (dt == -1 || abs (dt - d) != 1) ->
+        (res + 1) mod 2
+    | _, _, -1 when i mod x > 0 -> aux (i - 1) res (-1)
+    | _ -> res mod 2
+  in
+  aux i 0 (-1)
 
 let parse_file fp =
   let rec aux res x y =
@@ -83,16 +92,14 @@ let parse_file fp =
   in
   aux "" 0 0
 
-let rec reduce_i graph i acc = 0
-
 let () =
   let file_text, len_x, len_y = parse_file (open_in file_name) in
   let graph = create_graph file_text len_x len_y in
   let _ = parse_graph graph and raycast = raycast graph len_x in
   Array.fold_left
     (fun (acc, i) -> function
-      | _, _, -1 -> if raycast i then (acc + 1, i + 1) else (acc, i + 1)
-      | _ -> (acc, i + 1))
+      | _, _, -1 -> (acc + raycast i, i + 1)
+      | _, _, _ -> (acc, i + 1))
     (0, 0) graph
   |> fst
   |> Printf.printf "Enclosed tiles is %d\n"
