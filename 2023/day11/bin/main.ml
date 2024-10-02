8397c572fd4ce49bac5ea2bfb01a1090a8e56b36
chllamas <chrisllamas2000@gmail.com>
2024-10-02 13:26:03 -0700

    finished day11

--- 2023/day11/bin/main.ml
+++ 2023/day11/bin/main.ml
@@ -1,3 +1,5 @@
+let expanse_multiplier = 1_000_000
+
 let read_graph_from_input fp =
   let rec aux lc =
     match try Some (input_line fp) with End_of_file -> None with
@@ -6,73 +8,38 @@ let read_graph_from_input fp =
   in
   aux []
 
-let expand_graph lst =
-  let is_empty_row str = String.for_all (fun ch -> ch == '.') str
-  and is_empty_col lst i =
-    List.for_all (fun str -> Char.equal '.' str.[i]) lst
-  in
-  let expand_rows lst =
-    let rec aux st = function
-      | [] -> List.rev st
-      | x :: xs ->
-          if is_empty_row x then aux (x :: String.map (fun _ -> '.') x :: st) xs
-          else aux (x :: st) xs
-    in
-    aux [] lst
-  and expand_cols lst =
-    let n = String.length (List.hd lst) in
-    let rec aux i st =
-      match i with
-      | _ when i == n ->
-          List.map (fun lc -> lc |> List.rev |> List.to_seq |> String.of_seq) st
-      | _ ->
-          let is_empty = is_empty_col lst i in
-          aux (i + 1)
-            (List.map2
-               (fun src_str des_lst ->
-                 if is_empty then '.' :: src_str.[i] :: des_lst
-                 else src_str.[i] :: des_lst)
-               lst st)
-    in
-    aux 0 (List.map (fun _ -> []) lst)
-  in
-  lst |> expand_rows |> expand_cols
+let find_expanses graph =
+  let x_arr = ref [] and y_arr = ref [] in
+  Array.iteri
+    (fun i _ ->
+      if Array.for_all (fun row -> row.(i) == '.') graph then
+        x_arr := i :: !x_arr)
+    graph.(0);
+  Array.iteri
+    (fun i row ->
+      if Array.for_all (fun x -> x == '.') row then y_arr := i :: !y_arr)
+    graph;
+  (!x_arr, !y_arr)
 
-let solve_graph graph =
-  print_endline "Solving...";
-  let gy = Array.length graph
-  and gx = Array.length graph.(0)
-  and create_name x y = Printf.sprintf "%d,%d" x y in
-  Printf.printf "Working with a graph of size %d %d\n" gx gy;
-  let within_bounds x y = x >= 0 && x < gx && y >= 0 && y < gy in
-  let dist_between (sx, sy) (dx, dy) =
-    let visited = Hashtbl.create (gx * gy) and q = Queue.create () in
-    let exists x y = Hashtbl.mem visited (create_name x y) in
-    let mark dist x y =
-      if within_bounds x y && not (exists x y) then
-        let _ = Queue.push (x, y, dist + 1) q in
-        Hashtbl.add visited (create_name x y) true
-      else ()
-    in
-    let rec bfs () =
-      (* We not checking for empty queue because this will end by the time we reach the destination node *)
-      match Queue.pop q with
-      | x, y, dist when x == dx && y == dy -> dist
-      | x, y, dist ->
-          let m = mark dist in
-          m (x - 1) y;
-          m (x + 1) y;
-          m x (y - 1);
-          m x (y + 1);
-          bfs ()
-    in
-    mark (-1) sx sy;
-    bfs ()
+let solve_graph (graph : char array array)
+    ((x_expanses : int list), (y_expanses : int list)) =
+  let expansions (a, b) (x, y) =
+    List.fold_left
+      (fun acc cmp ->
+        if (cmp >= a && cmp <= x) || (cmp >= x && cmp <= a) then
+          acc + (expanse_multiplier - 1)
+        else acc)
+      0 x_expanses
+    + List.fold_left
+        (fun acc cmp ->
+          if (cmp >= y && cmp <= b) || (cmp >= b && cmp <= y) then
+            acc + (expanse_multiplier - 1)
+          else acc)
+        0 y_expanses
   in
-  let threads = ref []
-  and res_lock = Mutex.create ()
-  and res = ref 0
-  and finished_galaxies = ref 0 in
+  let dist_between ((sx, sy) as a) ((dx, dy) as b) =
+    abs (sx - dx) + abs (sy - dy) + expansions a b
+  in
   let num_galaxies =
     Array.fold_left
       (fun acc row ->
@@ -81,40 +48,15 @@ let solve_graph graph =
           acc row)
       0 graph
   in
-  let rec calc_distances galaxies i n =
-    let node = galaxies.(i) and
-    split = match n - 1 - i with
-  | x when x > 200 -> 4
-    | x when x > 150 -> 3
-    | x when x > 100 -> 2
-    | _ -> 1
-  in
-
-
-    | [] | [ _ ] -> List.iter (fun t -> Domain.join t) !threads
-    | t when List.length !threads == 4 ->
-        List.iter (fun t -> Domain.join t) !threads;
-        threads := [];
-        calc_distances t
+  let rec calc_distances res = function
+    | [] | [ _ ] -> res
     | galaxy_a :: xs ->
-        threads :=
-          Domain.spawn (fun () ->
-              let sum =
-                List.fold_left
-                  (fun acc galaxy_b -> acc + dist_between galaxy_a galaxy_b)
-                  0 xs
-              in
-              Mutex.lock res_lock;
-              res := !res + sum;
-              finished_galaxies := !finished_galaxies + 1;
-              Printf.printf "Progress %d of %d galaxies ... %.2f%%\n"
-                !finished_galaxies num_galaxies
-                (Float.div
-                   (Int.to_float !finished_galaxies)
-                   (Int.to_float num_galaxies));
-              Mutex.unlock res_lock)
-          :: !threads;
-        calc_distances xs
+        calc_distances
+          (res
+          + List.fold_left
+              (fun acc galaxy_b -> acc + dist_between galaxy_a galaxy_b)
+              0 xs)
+          xs
   in
   let index = ref 0 and galaxies = Array.init num_galaxies (fun _ -> (0, 0)) in
   Array.iteri
@@ -126,34 +68,13 @@ let solve_graph graph =
             index := !index + 1))
         row)
     graph;
-  calc_distances galaxies 0 num_galaxies;
-  !res
-
-(*
-let test_expanse graph =
-  try
-    if
-      List.for_all2
-        (fun str1 str2 -> String.equal str1 str2)
-        graph
-        (open_in "test_expanse.txt" |> read_graph_from_input)
-    then print_endline "The expanse was good"
-    else print_endline "The expanse was wrong"
-  with Invalid_argument _ ->
-    print_endline "Expanse failed due to differing lengths"
-*)
-
-(*
-let print_graph graph =
-  Array.iter
-    (fun str ->
-      print_string str;
-      print_newline ())
-    graph
-*)
+  calc_distances 0 (galaxies |> Array.to_list)
 
 let () =
-  open_in "input.txt" |> read_graph_from_input |> expand_graph
-  |> List.map (fun str -> str |> String.to_seq |> Array.of_seq)
-  |> Array.of_list |> solve_graph
+  let graph =
+    open_in "input.txt" |> read_graph_from_input
+    |> List.map (fun str -> str |> String.to_seq |> Array.of_seq)
+    |> Array.of_list
+  in
+  solve_graph graph (find_expanses graph)
   |> Printf.printf "The summation is %d\n"
