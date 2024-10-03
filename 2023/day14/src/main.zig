8543d2999acde14751929b33d0e5f6426e7abe8a
chllamas <chrisllamas2000@gmail.com>
2024-10-03 14:05:11 -0700

    updates

--- 2023/day14/src/main.zig
+++ 2023/day14/src/main.zig
@@ -1,4 +1,17 @@
 const std = @import("std");
+const pt = i32;
+const Position = struct { x: pt, y: pt };
+
+fn shift_graph(graph: [][]u8, shift: Position) void {
+    for (0.., graph) |y, *row| {
+        for (0.., row.*) |x, ch| {
+            if (ch == 'O') {
+                var cur: Position = .{ x, y };
+                var next: Position = .{ x + shift.x, y + shift.y };
+            }
+        }
+    }
+}
 
 pub fn main() !void {
     var allocator = std.heap.page_allocator;
@@ -12,11 +25,16 @@ pub fn main() !void {
 
     _ = try file.readAll(buffer);
 
-    var list = std.ArrayList(*[]u8).init(allocator);
+    var list = std.ArrayList([]u8).init(allocator);
     var iter = std.mem.splitScalar(u8, buffer, '\n');
     var cur = iter.next();
     while (cur != null) : (cur = iter.next()) {
-        var arr = try allocator.alloc(u8, cur.?.len);
-        try list.append(&arr);
+        const arr = try allocator.dupe(u8, cur.?);
+        try list.append(arr);
     }
+
+    const graph = try list.toOwnedSlice();
+    list.deinit();
+
+    shift_graph(graph);
 }
