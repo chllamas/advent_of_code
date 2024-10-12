fe3b55ec98e2923732c8552de3895bc55091dd99
chllamas <chrisllamas2000@gmail.com>
2024-10-12 15:25:39 -0700

    updates

--- 2023/day15/src/main.zig
+++ 2023/day15/src/main.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
 
+const Operation = struct {
+    key: []const u8,
+    focus: u64,
+};
+
 fn hash(str: []const u8) u64 {
     var res: u64 = 0;
     for (str) |ch| {
@@ -9,6 +14,67 @@ fn hash(str: []const u8) u64 {
         res %= 256;
     }
     return res;
+}
+
+fn find_index(arr: std.ArrayList(Operation), key: []const u8) ?usize {
+    for (0.., arr.items) |i, t|
+        if (std.mem.eql(u8, key, t.key))
+            return i;
+
+    return null;
+}
+
+fn calc(box_index: u64, slot_number: u64, focal_length: u64) u64 {
+    return (1 + box_index) * (1 + slot_number) * focal_length;
+}
+
+fn part1(buffer: []const u8) void {
+    var res: u64 = 0;
+    const sequence = std.mem.trimRight(u8, buffer, "\n");
+    var iter = std.mem.splitScalar(u8, sequence, ',');
+    while (iter.next()) |cur| {
+        res += hash(cur);
+    }
+
+    std.debug.print("Summations is: {d}\n", .{res});
+}
+
+fn part2(allocator: std.mem.Allocator, buffer: []const u8) !void {
+    const map = try allocator.alloc(std.ArrayList(Operation), 256);
+    for (0.., map) |i, _|
+        map[i] = std.ArrayList(Operation).init(allocator);
+
+    const sequence = std.mem.trimRight(u8, buffer, "\n");
+    var iter = std.mem.splitScalar(u8, sequence, ',');
+    while (iter.next()) |op| {
+        if (op[op.len - 1] == '-') {
+            const key = op[0 .. op.len - 1];
+            const box_num = hash(key);
+            if (find_index(map[box_num], key)) |slot|
+                _ = map[box_num].orderedRemove(slot);
+        } else {
+            var opIter = std.mem.splitScalar(u8, op, '=');
+            const key = opIter.next().?;
+            const focus: u64 = try std.fmt.parseInt(u64, opIter.next().?, 10);
+            const box_num = hash(key);
+            if (find_index(map[box_num], key)) |slot|
+                map[box_num].items[slot].focus = focus
+            else
+                try map[box_num].append(.{ .key = key, .focus = focus });
+        }
+    }
+
+    var res: u64 = 0;
+    for (0.., map) |box_num, box| {
+        for (0.., box.items) |slot, t| {
+            res += calc(@intCast(box_num), @intCast(slot), t.focus);
+        }
+    }
+    std.debug.print("Total power is: {d}\n", .{res});
+
+    for (0.., map) |i, _|
+        map[i].deinit();
+    allocator.free(map);
 }
 
 pub fn main() !void {
@@ -24,12 +90,5 @@ pub fn main() !void {
 
     _ = try file.readAll(buffer);
 
-    var res: u64 = 0;
-    const sequence = std.mem.trimRight(u8, buffer, "\n");
-    var iter = std.mem.splitScalar(u8, sequence, ',');
-    while (iter.next()) |cur| {
-        res += hash(cur);
-    }
-
-    std.debug.print("Summations is: {d}\n", .{res});
+    try part2(allocator, buffer);
 }
