c527c7934ed14c9f509ac7a2f5ff96b55388f203
chllamas <chrisllamas2000@gmail.com>
2024-10-13 00:30:19 -0700

    updates

--- 2023/day17/src/main.zig
+++ 2023/day17/src/main.zig
@@ -1,3 +1,34 @@
 const std = @import("std");
 
-pub fn main() !void {}
+const Coord = struct { x: usize, y: usize };
+
+const Node = struct {
+    last: ?Coord,
+    pos: Coord,
+    streak: u8,
+};
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
+    const file = try std.fs.cwd().openFile("input.txt", .{});
+    defer file.close();
+
+    const file_size = try file.getEndPos();
+
+    const buffer = try allocator.alloc(u8, file_size);
+    defer allocator.free(buffer);
+
+    _ = try file.readAll(buffer);
+
+    var array = std.ArrayList([]const u8).init(allocator);
+    defer array.deinit();
+
+    var iter = std.mem.splitScalar(u8, buffer, '\n');
+    while (iter.next()) |line| {
+        if (line.len > 0)
+            try array.append(line);
+    }
+
+    const map = try array.toOwnedSlice();
+}
