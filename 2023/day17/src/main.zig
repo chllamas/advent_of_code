8892508791c9cf936ab6865780759626e226caaf
chllamas <chrisllamas2000@gmail.com>
2024-10-15 14:20:03 -0700

    updates

--- 2023/day17/src/main.zig
+++ 2023/day17/src/main.zig
@@ -1,25 +1,19 @@
 const std = @import("std");
 
 const Direction = enum { up, left, right, down };
+const Node = struct { x: usize, y: usize };
 
-const Node = struct {
-    pos: .{ u32, u32 },
-    px: usize,
-    py: usize,
-    dir: ?Direction,
-    streak: u8,
-};
-
-fn nextToVisit(to_visit: []Node) ?u64 {
-    var result: ?u64 = null;
-    var res_dist: u64 = 0;
-    for (0.., to_visit) |i, node| {
-        if (node.dist != null and (result == null or node.dist.? < res_dist)) {
-            result = i;
-            res_dist = node.dist.?;
+fn nextToVisit(unvisited_nodes: []const Node, distance_table: []const []const ?u32) ?usize {
+    var index: ?usize = null;
+    var res_dist: u32 = std.math.maxInt(u32);
+    for (0.., unvisited_nodes) |i, node| {
+        const node_distance = distance_table[node.pos.y][node.pos.x];
+        if (node_distance != null and (index == null or node_distance.? < res_dist)) {
+            index = i;
+            res_dist = node_distance.?;
         }
     }
-    return result;
+    return index;
 }
 
 pub fn main() !void {
@@ -47,43 +41,49 @@ pub fn main() !void {
     const map = try array.toOwnedSlice();
     defer allocator.free(map);
 
-    const distances = try allocator.alloc([]?u32, map.len);
-    defer allocator.free(distances);
-    for (0..distances.len) |i|
-        distances[i] = try allocator.alloc(?u32, map[0].len);
+    // TODO: This table needs to include direction and its streak
+    var distances_table = try allocator.alloc([]?u32, map.len);
+    defer allocator.free(distances_table);
+    for (0..distances_table.len) |i| {
+        distances_table[i] = try allocator.alloc(?u32, map[0].len);
+        for (distances_table[i]) |*t|
+            t.* = null;
+    }
 
-    const visited = std.ArrayList(Node).init(allocator);
+    var visited = std.ArrayList(Node).init(allocator);
     defer visited.deinit();
 
-    const non_visited = std.ArrayList(Node).init(allocator);
-    defer non_visited.deinit();
+    var not_visited = std.ArrayList(Node).init(allocator);
+    defer not_visited.deinit();
 
     for (0..map.len) |y| {
         for (0..map[0].len) |x| {
-            try non_visited.append(.{
-                .pos = .{},
+            try not_visited.append(.{
+                .pos = .{ .x = x, .y = y }, // TODO: Update to include the new table
+                .streak = 0,
                 .dir = null,
-                .dist = null,
-                .streak = null,
             });
-            const t = non_visited.items[0];
-            const y = t.pos;
         }
     }
 
-    non_visited.items[0] = .{
+    not_visited.items[0] = .{
         .pos = .{ .x = 0, .y = 0 },
-        .dir = null,
-        .dist = @intCast(map[0][0]),
+        .dir = null, // TODO: Update to use the new table
         .streak = 1,
     };
+    distances_table[0][0] = @intCast(map[0][0] - '0');
 
-    while (nextToVisit(non_visited)) |i| {
-        const node = visited.swapRemove(i);
-        if (node.pos.x > 0) {}
+    while (nextToVisit(not_visited.items, distances_table)) |i| {
+        const node = not_visited.swapRemove(i);
+        // left node
+        if (node.pos.x > 0 and (node.dir == null or node.dir.? != Direction.left or node.streak != 3)) {}
+
+        // right node
+        // up node
+        // down node
         try visited.append(node);
     }
 
-    for (distances) |d|
+    for (distances_table) |d|
         allocator.free(d);
 }
