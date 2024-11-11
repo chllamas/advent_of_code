baec8a4fa9f3898801aead5bfa637664309551a5
chllamas <chrisllamas2000@gmail.com>
2024-11-11 00:40:29 -0800

    updates

--- 2020/day15/src/main.zig
+++ 2020/day15/src/main.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
 
-const num_turns = 10;
+const num_turns = 30000000;
 
 const test_input = [_]u32{ 0, 3, 6 };
 const real_input = [_]u32{ 0, 8, 15, 2, 12, 1, 4 };
 
 pub fn main() !void {
-    const input: []const u32 = test_input[0..];
+    const input: []const u32 = real_input[0..];
     const allocator = std.heap.page_allocator;
 
     var spoken_numbers = std.AutoHashMap(u32, u32).init(allocator);
@@ -15,22 +15,21 @@ pub fn main() !void {
     var last_spoken: u32 = 0;
     for (0.., input) |i, x| {
         const turn: u32 = @intCast(i);
-        try spoken_numbers.putNoClobber(x, turn + 1);
         last_spoken = x;
+        if (i < input.len - 1)
+            try spoken_numbers.putNoClobber(x, turn + 1);
     }
 
     for (input.len + 1..num_turns + 1) |i| {
         const turn: u32 = @intCast(i);
-        const last_time_yelled = spoken_numbers.get(last_spoken);
+        const t = last_spoken;
+        const last_time_yelled = spoken_numbers.get(t);
         if (last_time_yelled == null) {
-            std.debug.print("Turn {}: {} is new, yelling 0\n", .{ turn, last_spoken });
             last_spoken = 0;
-            try spoken_numbers.put(0, turn);
         } else {
-            std.debug.print("Turn {}: {} was yelled at {}, yelling {}\n", .{ turn, last_spoken, last_time_yelled.?, turn - last_time_yelled.? });
-            last_spoken = turn - last_time_yelled.?;
-            try spoken_numbers.put(last_spoken, turn);
+            last_spoken = turn - last_time_yelled.? - 1;
         }
+        try spoken_numbers.put(t, turn - 1);
     }
 
     std.debug.print("Game end: {}\n", .{last_spoken});
