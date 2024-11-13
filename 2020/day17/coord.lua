524428c108840a8d0cbb37481030f5b4c1a97a60
chllamas <chrisllamas2000@gmail.com>
2024-11-13 09:15:21 -0800

    updates

---
+++ 2020/day17/coord.lua
@@ -0,0 +1,10 @@
+local coord = {}
+
+--- Create new coord
+--- @param x number
+--- @param y number
+--- @param z number
+--- @return @{Coord}
+function coord.new(x, y, z) end
+
+return coord
--- 2020/day17/main.lua
+++ 2020/day17/main.lua
@@ -1,1 +1,45 @@
-print("hello, world!")
+local test_input = {
+	".#.",
+	"..#",
+	"###",
+}
+
+local real_input = {
+	".##...#.",
+	".#.###..",
+	"..##.#.#",
+	"##...#.#",
+	"#..#...#",
+	"#..###..",
+	".##.####",
+	"..#####.",
+}
+
+local input = test_input
+
+local function remainsActive(coord)
+	local count = 0
+	for _, active_cube in ipairs(ActiveCubes) do
+	end
+	return false
+end
+
+ActiveCubes = {}
+for y = 1, #input do
+	local row = input[y]
+	for x = 1, row:len() do
+		local ch = row:sub(x, x)
+		if ch == "#" then
+			table.insert(ActiveCubes, { x, y, 0 })
+		end
+	end
+end
+
+for _ = 1, 6 do
+	local buffer = {}
+
+	for _, cube in ipairs(active_cubes) do
+	end
+
+	active_cubes = buffer
+end
