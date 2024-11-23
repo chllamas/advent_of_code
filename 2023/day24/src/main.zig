2048f56a60f8408e3008eb9676040244a55e4c06
chllamas <chrisllamas2000@gmail.com>
2024-11-23 13:30:54 -0800

    updates

--- 2023/day24/src/main.zig
+++ 2023/day24/src/main.zig
@@ -1,11 +1,46 @@
 const std = @import("std");
 
-const CoordNum = f64;
+const Matrix2x1 = struct {
+    x: f64,
+    y: f64,
+};
+
+const Matrix2x2 = struct {
+    a: f64,
+    b: f64,
+    c: f64,
+    d: f64,
+
+    fn inv(self: Matrix2x2) ?Matrix2x2 {
+        const d = self.det();
+
+        return if (d == 0)
+            null
+        else
+            .{
+                .a = self.d / d,
+                .b = (-self.b) / d,
+                .c = (-self.c) / d,
+                .d = self.a / d,
+            };
+    }
+
+    fn det(self: Matrix2x2) f64 {
+        return (self.a * self.d) - (self.b * self.c);
+    }
+
+    fn mult(self: Matrix2x2, other: Matrix2x1) Matrix2x1 {
+        return .{
+            .x = (self.a * other.x) + (self.b * other.y),
+            .y = (self.c * other.x) + (self.d * other.y),
+        };
+    }
+};
 
 const Coord = struct {
-    x: CoordNum,
-    y: CoordNum,
-    // z: CoordNum,
+    x: f64,
+    y: f64,
+    // z: f64,
 };
 
 const Vector = struct {
@@ -16,10 +51,12 @@ const Vector = struct {
 // assumes buffer is formatted as "%d, %d, %d"
 fn parseCoord(buffer: []const u8) !Coord {
     var iter = std.mem.splitSequence(u8, buffer, ", ");
+    const x = std.mem.trim(u8, iter.next().?, " ");
+    const y = std.mem.trim(u8, iter.next().?, " ");
     return Coord{
-        .x = try std.fmt.parseInt(CoordNum, iter.next().?, 10),
-        .y = try std.fmt.parseInt(CoordNum, iter.next().?, 10),
-        // .z = try std.fmt.parseInt(CoordNum, iter.next().?, 10),
+        .x = try std.fmt.parseFloat(f64, x),
+        .y = try std.fmt.parseFloat(f64, y),
+        // .z = try std.fmt.parseInt(f64, iter.next().?, 10),
     };
 }
 
@@ -35,7 +72,9 @@ fn parseVector(buffer: []const u8) !Vector {
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
-    const file = try std.fs.cwd().openFile("test.txt", .{});
+    // answer is between 12k and 18k
+    const file = try std.fs.cwd().openFile("input.txt", .{});
+    const min_max = Coord{ .x = 200000000000000, .y = 400000000000000 }; // NOTE: change for test or input
     defer file.close();
 
     const buffer = try allocator.alloc(u8, try file.getEndPos());
@@ -52,6 +91,7 @@ pub fn main() !void {
     }
 
     const vectors = try vectors_list.toOwnedSlice();
+    var count: u64 = 0;
 
     // NOTE: Not iterating to last one since last one doesn't match with any other after
     for (0..vectors.len - 1) |i| {
@@ -63,18 +103,23 @@ pub fn main() !void {
             const d1 = vec1.direction;
             const d2 = vec2.direction;
 
-            const a_matrix = [4]CoordNum{ d1.x, -d2.y, d1.y, -d2.x }; // 2x2 matrix
-            const b_matrix = [2]CoordNum{ p2.x - p1.x, p2.y - p1.y }; // 1 x 2 matrix
+            const a_inverse =
+                (Matrix2x2{ .a = d1.x, .b = (-d2.x), .c = d1.y, .d = (-d2.y) })
+                .inv();
 
-            const determinant: CoordNum = (a_matrix[0] * a_matrix[2]) - (a_matrix[1] * a_matrix[3]);
-            if (determinant == 0)
-                continue;
+            if (a_inverse == null) continue;
 
-            const a_inverse = [4]CoordNum{ a_matrix[0] / determinant, a_matrix[1] / determinant, a_matrix[2] / determinant, a_matrix[3] / determinant };
+            const t_s = a_inverse.?
+                .mult(.{ .x = p2.x - p1.x, .y = p2.y - p2.x });
 
-            // TODO: Check if they intersect within the thingy
-            // Gaussian matrices
-            // Matrix inversion just look at what was generated
+            if (t_s.x < 0 or t_s.y < 0) continue;
+
+            const intersection = Matrix2x1{ .x = p1.x + (d1.x * t_s.x), .y = p1.y + (d1.y * t_s.x) };
+
+            if (intersection.x >= min_max.x and intersection.x <= min_max.y and intersection.y >= min_max.x and intersection.y <= min_max.y)
+                count += 1;
         }
     }
+
+    std.debug.print("Final count: {}\n", .{count});
 }
