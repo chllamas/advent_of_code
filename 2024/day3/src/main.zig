0ba218fc3afc560b782858bc1cdd6530e354cc8a
chllamas <chrisllamas2000@gmail.com>
2024-12-02 23:10:43 -0800

    updates

---
+++ 2024/day3/src/main.zig
@@ -0,0 +1,49 @@
+const std = @import("std");
+
+fn part1(buffer: []const u8) !void {
+    var sum: u32 = 0;
+    var iter = std.mem.splitSequence(u8, buffer, "mul(");
+    iter_loop: while (iter.next()) |str| {
+        var first_num = true;
+        var num_a: u32 = 0;
+        var num_b: u32 = 0;
+        for (str) |ch| {
+            if (first_num) {
+                if (ch >= '0' and ch <= '9') {
+                    const val: u32 = @intCast(ch - '0');
+                    num_a = num_a * 10 + val;
+                } else if (ch == ',') {
+                    first_num = false;
+                } else {
+                    continue :iter_loop;
+                }
+            } else {
+                if (ch >= '0' and ch <= '9') {
+                    const val: u32 = @intCast(ch - '0');
+                    num_b = num_b * 10 + val;
+                } else {
+                    if (ch == ')')
+                        sum += num_a * num_b;
+                    continue :iter_loop;
+                }
+            }
+        }
+    }
+    std.debug.print("Result: {}\n", .{sum});
+}
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
+    const file = try std.fs.cwd().openFile("input.txt", .{});
+    defer file.close();
+
+    const file_size = try file.getEndPos();
+
+    const buffer = try allocator.alloc(u8, file_size);
+    defer allocator.free(buffer);
+
+    _ = try file.readAll(buffer);
+
+    try part1(buffer);
+}
