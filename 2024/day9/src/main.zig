835b17be22eb576782fde815086d541fb9580f2d
chllamas <chrisllamas2000@gmail.com>
2024-12-11 12:25:39 -0800

    updates

--- 2024/day9/src/main.zig
+++ 2024/day9/src/main.zig
@@ -1,5 +1,18 @@
 const std = @import("std");
 
+const ChunkType = enum { id, null };
+
+const Chunk = union(ChunkType) {
+    id: struct {
+        value: u32,
+        length: u8,
+    },
+
+    null: struct {
+        length: u8,
+    },
+};
+
 fn createPreprocessedFormat(allocator: std.mem.Allocator, buffer: []const u8) ![]?u32 {
     var i: usize = 0;
     var id: u32 = 0;
@@ -37,7 +50,7 @@ fn processFiles(arr: []?u32) []?u32 {
         var t: usize = i - 1;
         while (arr[t] == arr[i]) : (t -= 1) {}
         defer i = t;
-        const slice = arr[t + 1 .. i + 1];
+        // const slice = arr[t + 1 .. i + 1];
         // TODO: Now try to move the slice back to a null slot of this size
     }
 }
@@ -54,22 +67,63 @@ fn part1(allocator: std.mem.Allocator, buffer: []const u8) !void {
 }
 
 fn part2(allocator: std.mem.Allocator, buffer: []const u8) !void {
-    const arr = try createPreprocessedFormat(allocator, buffer);
-    processFiles(arr);
-    var sum: u64 = 0;
-    for (0.., arr) |i, ch| {
-        if (ch) |t| {
-            const val: u32 = @intCast(i);
-            sum += val * t;
+    var i: usize = 0;
+    var id: u32 = 0;
+    var list = std.ArrayList(Chunk).init(allocator);
+    while (i < buffer.len - 1) : (i += 2) {
+        defer id += 1;
+        try list.append(.{ .id = .{
+            .value = id,
+            .length = buffer[i] - '0',
+        } });
+        try list.append(.{ .null = .{
+            .length = buffer[i + 1] - '0',
+        } });
+    }
+    try list.append(.{ .id = .{
+        .value = id,
+        .length = buffer[buffer.len - 1] - '0',
+    } });
+
+    const arr = try list.toOwnedSlice();
+    var _i: usize = 0;
+    while (_i < arr.len - 1) : (_i += 1) {
+        const idx: usize = arr.len - 1 - _i;
+        switch (arr[idx]) {
+            .id => |node| {
+                for (0..idx) |swp_idx| {
+                    switch (arr[swp_idx]) {
+                        .null => |swp_node| {
+                            if (swp_node.length == node.length) {
+                                const t = arr[swp_idx];
+                                arr[swp_idx] = arr[idx];
+                                arr[idx] = t;
+                            } else if (swp_node.length > node.length) {}
+                        },
+                        else => continue,
+                    }
+                }
+            },
+            else => continue,
         }
     }
-    std.debug.print("{}\n", .{sum});
+
+    var sum: u64 = 0;
+    var cur_id: u32 = 0;
+    for (arr) |chunk| switch (chunk) {
+        .id => |node| {
+            sum += node.value * (@divExact(node.length * ((2 * cur_id) + (node.length - 1)), 2));
+            cur_id += node.length;
+        },
+        .null => |node| cur_id += node.length,
+    };
+    std.debug.print("Result: {}\n", .{sum});
 }
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
-    const file = try std.fs.cwd().openFile("input.txt", .{});
+    const file = try std.fs.cwd().openFile("test.txt", .{});
     defer file.close();
 
     const file_size = try file.getEndPos();
