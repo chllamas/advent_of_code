7ca9e254041c87a6e91970d757a121882b8418a8
chllamas <chrisllamas2000@gmail.com>
2024-12-18 00:11:08 -0800

    updates

---
+++ 2024/day18/src/main.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+
+fn part1(allocator: std.mem.Allocator, buffer: []const u8) !void {}
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
+    const file = try std.fs.cwd().openFile("test.txt", .{});
+    defer file.close();
+
+    const file_size = try file.getEndPos();
+
+    const buffer = try allocator.alloc(u8, file_size);
+    defer allocator.free(buffer);
+
+    _ = try file.readAll(buffer);
+
+    try part1(allocator, std.mem.trimRight(u8, buffer, "\n"));
+    // try part2(allocator, std.mem.trimRight(u8, buffer, "\n"));
+}
